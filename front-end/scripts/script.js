39d9eda59190bf4c50284edfff2de1861f01cf45
maisaborchardt <maisaborchardt@gmail.com>
2020-08-31 20:07:31 -0300

    front-end

---
+++ front-end/scripts/script.js
@@ -0,0 +1,23 @@
+$(function() {
+    $.ajax({
+        url: 'http://localhost:5000/index_musica',
+        method: 'GET',
+        dataType: 'json', 
+        success: listarMusicas, 
+        error: function() {
+            alert("ERRO");
+        }
+    });
+
+    function listarMusicas(musicas) {
+        for (musica of musicas) { 
+            novaLinha = `<tr> 
+                        <td>${musica.nome}</td> 
+                        <td>${musica.banda}</td> 
+                        <td>${musica.data}</td> 
+                        <td>${musica.duracao}</td> 
+                      </tr>`;
+            $('#tabelaMusica').append(novaLinha); 
+        }
+    }
+});
